e9152e6966ef6ab99ec7d4f7934c12e272b51353
Rooobins <qiantun@msn.cn>
2018-06-20 18:24:01 +0800

    补充

--- loadDataToMySQL.sql
+++ loadDataToMySQL.sql
@@ -1,11 +1,34 @@
-use spiderData;
-DROP EVENT IF EXISTS loadDataToMySQL
-CREATE EVENT loadDataToMySQL
-ON SCHEDULE EVERY 8 HOURS STARTS NOW()
+use spiderData
+CREATE EVENT IF NOT EXISTS loadDataToMySQL
+ON SCHEDULE EVERY 28800 second
 ON COMPLETION PRESERVE
-DO 
-BEGIN
-    load data local infile "/root/end1.txt" into table operrecord_18061901 fields terminated by ' ';
-    load data local infile "/root/end2.txt" into table operrecord_18061901 fields terminated by ' ';
-END 
+DO load data local infile "/root/end1.txt" into table operrecord_18061901 fields terminated by ' ';
 
+
+
+
+
+DELIMITER $$
+ 
+DROP PROCEDURE IF EXISTS loaddata $$
+CREATE PROCEDURE loaddata (IN data_file VARCHAR(1000))
+BEGIN
+    DECLARE load_sql VARCHAR(4000);
+ 
+    DROP TABLE IF EXISTS testload;
+    CREATE TABLE testload (name char(10), age tinyint, city char(30));
+    INSERT INTO testload VALUES("Bob",27,"New York");
+ 
+    SET load_sql=CONCAT('LOAD DATA LOCAL INFILE ',
+                        '"', data_file, '" ',
+                        'INTO TABLE testload ',
+                        'FIELDS TERMINATED BY ","');
+    SET @sql=load_sql;
+    PREPARE s1 FROM @sql;
+    EXECUTE s1;
+    DEALLOCATE PREPARE s1;
+END$$
+ 
+DELIMITER ;
+ 
+CALL loaddata("/tmp/testdata.txt");
